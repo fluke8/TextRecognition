2854e084c693f7e1efbca436248b011e9c2026b6
fluke8 <frist410@gmail.com>
2024-09-12 17:36:07 +0700

    пофиксил скачивание модели и добавил перенос модели на основной девайс

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ import os
 from src.utils import *
 from src.config import *
 from src.model import CRNN
+from src.download_model import download_model
 
 def main(image_path):
     model = CRNN(NUM_CLASSES, CHANNELS)
+
+    if not os.path.isfile(MODEL_PATH):
+        download_model()
+
     model.load_state_dict(torch.load(MODEL_PATH, weights_only=False, map_location=torch.device(DEVICE)).state_dict())
+    model.to(DEVICE)
+    model.eval()
 
     if os.path.isdir(image_path):
         image_paths = [os.path.join(image_path, filename) for filename in os.listdir(image_path)]
@@ -31,7 +38,7 @@ def main(image_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some images.')
-    parser.add_argument('--image_path', type=str, required=True, help='Path to the image(s)')
+    parser.add_argument('--image-path', type=str, required=True, help='Path to the image(s)')
     
     args = parser.parse_args()
     main(args.image_path)
--- src/download_model.py
+++ src/download_model.py
@@ -1,16 +1,17 @@
 import os
 import requests
-from config import MODEL_PATH, MODEL_DIR, MODEL_URL
+from .config import MODEL_PATH, MODEL_DIR, MODEL_URL
 
-response = requests.get(MODEL_URL)
+def download_model():
+    response = requests.get(MODEL_URL)
 
-if not os.path.exists(MODEL_DIR):
-    os.mkdir(MODEL_DIR)
+    if not os.path.exists(MODEL_DIR):
+        os.mkdir(MODEL_DIR)
 
-if response.status_code == 200:
-    with open(MODEL_PATH, 'wb') as file:
-        file.write(response.content)
-    print("Модель успешно скачана и сохранена в:", MODEL_PATH)
-else:
-    print("Ошибка при скачивании модели:", response.status_code)
+    if response.status_code == 200:
+        with open(MODEL_PATH, 'wb') as file:
+            file.write(response.content)
+        print("Модель успешно скачана и сохранена в:", MODEL_PATH)
+    else:
+        print("Ошибка при скачивании модели:", response.status_code)
 
